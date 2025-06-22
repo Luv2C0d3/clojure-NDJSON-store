6be722e34168e411e721d8e083aaa8a5a108b14a
Pierre Jacomet <pierrj@hotmail.com>
2025-06-22 17:02:05 -0300

    Changed the main app, such that I can have 2 repositories by passing in the repo name for the file to use

--- src/mi_app_clj/core.clj
+++ src/mi_app_clj/core.clj
@@ -3,35 +3,56 @@
             [repository.bubas :as bubas])
   (:gen-class))
 
+(defonce bubas-repository (atom nil))
+(defonce bubas-repository-2 (atom nil))
+
 (defn create-example-bear!
   "Creates an example bear entry"
-  []
+  [repo-atom]
   (let [bear-id "bear-123"
         client-id "client-456"
         scope "read write"
-        expires-at (+ (System/currentTimeMillis) (* 3600 1000))] ; 1 hour from now
+        expires-at (+ (System/currentTimeMillis) (* 3600 1000)) ; 1 hour from now
+        [new-repo entry] (bubas/store-bear! @repo-atom bear-id client-id scope expires-at)]
+    (reset! repo-atom new-repo)
     (println "Creating bear:" bear-id)
-    (bubas/store-bear! bear-id client-id scope expires-at)))
+    entry))
 
 (defn create-example-grizzly!
   "Creates an example grizzly entry"
-  []
+  [repo-atom]
   (let [grizzly-id "grizzly-789"
         client-id "client-456"
-        scope "read"]
+        scope "read"
+        [new-repo entry] (bubas/store-grizzly! @repo-atom grizzly-id client-id scope)]
+    (reset! repo-atom new-repo)
     (println "Creating grizzly:" grizzly-id)
-    (bubas/store-grizzly! grizzly-id client-id scope)))
+    entry))
 
 (defn -main
   "Main function that runs when starting the application"
   [& args]
+  ;; Initialize the repository
+  (reset! bubas-repository (bubas/create-bubas-repository "ositos.nosql"))
+  (reset! bubas-repository-2 (bubas/create-bubas-repository "yoggies.ndjson"))
+
   (println "¡Bienvenido a mi aplicación de consola!")
   (println "Argumentos recibidos:" (or args "ninguno"))
   (println "Versión:" (utils/version))
   
   ;; Create example bear and grizzly
-  (println "\nCreating example bear and grizzly:")
-  (let [bear (create-example-bear!)
-        grizzly (create-example-grizzly!)]
+  (println "\nCreating example bear and grizzly in first repo:")
+  (let [bear (create-example-bear! bubas-repository)
+        grizzly (create-example-grizzly! bubas-repository)
+        bear2 (create-example-bear! bubas-repository-2)
+        grizzly2 (create-example-grizzly! bubas-repository-2)]
+    (println "Created bear:" bear)
+    (println "Created grizzly:" grizzly)
+    (println "Created bear2:" bear2)
+    (println "Created grizzly2:" grizzly2))
+
+  (println "\nCreating example bear and grizzly in second repo:")
+  (let [bear (create-example-bear! bubas-repository-2)
+        grizzly (create-example-grizzly! bubas-repository-2)]
     (println "Created bear:" bear)
     (println "Created grizzly:" grizzly)))
--- src/repository/bubas.clj
+++ src/repository/bubas.clj
@@ -3,52 +3,52 @@
             [clojure.string :as str]
             [clojure.tools.logging :as log]))
 
-;; Define the token repository with two primary keys: access_token and refresh_token
-(def bubas-repository
-  (create-repository
-   "resources/bubas.nosql" 
-   ["bear" "grizzly"])) ;; Two primary keys for O(1) lookups
+(defn create-bubas-repository
+  "Creates a new bubas repository"
+  ([] (create-bubas-repository "resources/bubas.nosql"))
+  ([filename]
+   (create-repository filename ["bear" "grizzly"])))
 
 (defn find-bubas-by-grizzly
   "Find a token entry by its access_token"
-  [grizzly]
-  (find-by-key bubas-repository "grizzly" grizzly))
+  [repo grizzly]
+  (find-by-key repo "grizzly" grizzly))
 
 (defn find-bubas-by-bear
   "Find a bear entry by its bear"
-  [bear]
-  (let [bear-entry (find-by-key bubas-repository "bear" bear)]
+  [repo bear]
+  (let [bear-entry (find-by-key repo "bear" bear)]
     (log/info "-->in bubas::find-bubas-by-bear: Found bear data:" (pr-str bear-entry))
     (log/info "-->in bubas::find-bubas-by-bear: Bear:" bear)
     bear-entry))
 
 (defn store-bear!
   "Store a new access bear"
-  [bear client-id scope expires-at]
-  (let [bubas-entry {"bear" bear      ; FIXED: Use string keys for consistency
+  [repo bear client-id scope expires-at]
+  (let [bubas-entry {"bear" bear
                      "client_id" client-id
                      "scope" (str/split scope #" ")
-                     "expires_at" expires-at}]
-    (alter-var-root #'bubas-repository #(add! % bubas-entry))  ; FIXED: Proper var mutation
-    bubas-entry))
+                     "expires_at" expires-at}
+        new-repo (add! repo bubas-entry)]
+    [new-repo bubas-entry]))
 
 (defn store-grizzly!
   "Store a new grizzly with its associated data"
-  [grizzly client-id scope]
-  (let [bubas-entry {"grizzly" grizzly    ; FIXED: Use string keys for consistency
+  [repo grizzly client-id scope]
+  (let [bubas-entry {"grizzly" grizzly
                      "client_id" client-id
-                     "scope" (str/split scope #" ")}]
-    (alter-var-root #'bubas-repository #(add! % bubas-entry))  ; FIXED: Proper var mutation
-    bubas-entry))
+                     "scope" (str/split scope #" ")}
+        new-repo (add! repo bubas-entry)]
+    [new-repo bubas-entry]))
 
 (defn remove-bubas!
   "Remove a bubas entry from the repository by either bear or grizzly"
-  [bubas]
-  (when-let [entry (or (find-bubas-by-bear (get bubas "bear"))  ; FIXED: Use string keys
-                       (find-bubas-by-grizzly (get bubas "grizzly")))]
+  [repo bubas]
+  (when-let [entry (or (find-bubas-by-bear repo (get bubas "bear"))
+                       (find-bubas-by-grizzly repo (get bubas "grizzly")))]
     (let [key-name (if (get bubas "bear") "bear" "grizzly")
           key-value (if (get bubas "bear")
                       (get bubas "bear")
-                      (get bubas "grizzly"))]
-      (alter-var-root #'bubas-repository #(delete! % key-name key-value))  ; FIXED: Proper var mutation
-      entry)))
+                      (get bubas "grizzly"))
+          new-repo (delete! repo key-name key-value)]
+      [new-repo entry])))
