019db5ae0aef153dca2d86cb73548c52d27e0e13
Pierre Jacomet <pierrj@hotmail.com>
2025-06-23 20:44:11 -0300

    Token repository working

---
+++ test/repository/token.clj
@@ -0,0 +1,75 @@
+(ns repository.token
+  (:require [repository.ndjson :refer [create-repository find-by-key add! delete!]]
+            [clojure.string :as str]
+            [clojure.tools.logging :as log]))
+
+;; Define the token repository with two primary keys: access_token and refresh_token
+(def token-repository
+  (create-repository
+   "resources/tokens.nosql"  ;; Store tokens in tokens.nosql
+   [:access_token :refresh_token])) ;; Two primary keys for O(1) lookups
+
+
+(defn create-token-repository
+  "Creates a new token repository"
+  ([] (create-token-repository "resources/tokens.nosql"))
+  ([filename]
+   (create-repository filename [:access_token :refresh_token])))
+
+
+(defn find-token-by-access-token
+  "Find a token entry by its access_token"
+  [repo access-token]
+  (find-by-key repo :access_token access-token))
+
+
+(defn find-token-by-refresh-token
+  "Find a token entry by its refresh_token"
+  [repo refresh-token]
+  (find-by-key repo :refresh_token refresh-token))
+
+(defn store-access-token!
+  "Store a new access token with its associated data"
+  [repo access-token client-id scope expires-at]
+  (let [token-entry {:access_token access-token
+                     :client_id client-id
+                     :scope (str/split scope #" ")
+                     :expires_at expires-at}
+        new-repo (add! repo token-entry)]
+    [new-repo token-entry]))
+
+(defn store-refresh-token!
+  "Store a new refresh token with its associated data"
+  [repo refresh-token client-id scope]
+  (let [token-entry {:refresh_token refresh-token
+                     :client_id client-id
+                     :scope (str/split scope #" ")}
+        new-repo (add! repo token-entry)]
+    [new-repo token-entry]))
+
+(defn- remove-token-by-type!
+  "Internal helper to remove a token by its type"
+  [repo token-type token-id]
+  (if-let [entry (find-by-key repo token-type token-id)]
+    (let [new-repo (delete! repo token-type token-id)]
+      [new-repo entry])
+    [repo nil]))
+
+(defn remove-token!
+  "Remove a token entry from the repository by either access_token or refresh_token"
+  [repo token-id]
+  (let [[new-repo entry] (or (remove-token-by-type! repo :access_token token-id)
+                            (remove-token-by-type! repo :refresh_token token-id))]
+    (or new-repo repo)))
+
+(defn remove-access-token!
+  "Remove an access token entry from the repository"
+  [repo token-id]
+  (let [[new-repo _] (remove-token-by-type! repo :access_token token-id)]
+    (or new-repo repo)))
+
+(defn remove-refresh-token!
+  "Remove a refresh token entry from the repository"
+  [repo token-id]
+  (let [[new-repo _] (remove-token-by-type! repo :refresh_token token-id)]
+    (or new-repo repo)))
---
+++ test/repository/tokens_test.clj
@@ -0,0 +1,124 @@
+(ns repository.tokens_test
+  (:require [clojure.test :refer :all]
+            [clojure.tools.logging :as log]
+            [repository.token :as token]
+            [clojure.java.io :as io]))
+
+(defn- cleanup-test-files! []
+  (doseq [file ["tokens1.nosql" "tokens2.ndjson" "tokens3.ndjson"]]
+    (when (.exists (io/file file))
+      (io/delete-file file))))
+
+(use-fixtures :each
+  (fn [test-fn]
+    (cleanup-test-files!)
+    (test-fn)
+    (cleanup-test-files!)))
+
+(defn- create-example-token!
+  [repo-atom token-num]
+  (let [token-id (str "token-" token-num)
+        client-id "client-456"
+        scope "read write"
+        expires-at (+ (System/currentTimeMillis) (* 3600 1000))
+        [new-repo entry] (token/store-access-token! @repo-atom token-id client-id scope expires-at)]
+    (reset! repo-atom new-repo)
+    entry))
+
+(defn- create-example-refresh-token!
+  [repo-atom refresh-token-num]
+  (let [refresh-token-id (str "refresh-token-" refresh-token-num)
+        client-id "client-456"
+        scope "read"
+        [new-repo entry] (token/store-refresh-token! @repo-atom refresh-token-id client-id scope)]
+    (reset! repo-atom new-repo)
+    entry))
+
+(defn- assert-repositories-match
+  "Helper function to compare two repositories' tokens and refresh tokens"
+  [repo1 repo2 expected-token-count expected-refresh-token-count]
+  (let [token-keys1 (-> repo1 :indexes (get :access_token) keys set)
+        token-keys2 (-> repo2 :indexes (get :access_token) keys set)
+        refresh-token-keys1 (-> repo1 :indexes (get :refresh_token) keys set)
+        refresh-token-keys2 (-> repo2 :indexes (get :refresh_token) keys set)]
+    (is (= token-keys1 token-keys2) "Token keys should match between repositories")
+    (is (= expected-token-count (count token-keys1)) "Should have correct number of tokens")
+    (is (= refresh-token-keys1 refresh-token-keys2) "Refresh token keys should match between repositories")
+    (is (= expected-refresh-token-count (count refresh-token-keys1)) "Should have correct number of refresh tokens")))
+
+(deftest test-repository-add-and-remove
+  (let [repo (atom (token/create-token-repository "tokens1.nosql"))]
+
+    ;; Add initial entries
+    (doseq [i (range 1 5)]
+      (create-example-token! repo i))
+    (doseq [i (range 1 4)]
+      (create-example-refresh-token! repo i))
+
+    ;; Remove some entries
+    (let [new-repo (token/remove-token! @repo "token-1")]
+      (reset! repo new-repo))
+    (let [new-repo (token/remove-token! @repo "token-2")]
+      (reset! repo new-repo))
+    (let [new-repo (token/remove-refresh-token! @repo "refresh-token-1")]
+      (reset! repo new-repo))
+
+    ;; Test counts in original repository
+    (testing "Repository after removals"
+      (let [token-keys (-> @repo :indexes (get :access_token) keys set)
+            refresh-token-keys (-> @repo :indexes (get :refresh_token) keys set)]
+        (is (= 2 (count token-keys)) "Should have 2 tokens remaining")
+        (is (= 2 (count refresh-token-keys)) "Should have 2 refresh tokens remaining")
+        (is (= #{"token-3" "token-4"} token-keys) "Should have correct tokens")
+        (is (= #{"refresh-token-2" "refresh-token-3"} refresh-token-keys) "Should have correct refresh tokens")))
+
+    ;; Load repository from file and compare
+    (testing "Repository reloaded from file"
+      (let [loaded-repo (token/create-token-repository "tokens1.nosql")]
+        (assert-repositories-match @repo loaded-repo 2 2)))
+
+    ;; Add more entries
+    (create-example-token! repo 5)  ;; Add one more token
+    (doseq [i (range 4 7)]         ;; Add three more refresh tokens
+      (create-example-refresh-token! repo i))
+
+    ;; Test counts after adding more entries
+    (testing "Repository after adding more entries"
+      (let [token-keys (-> @repo :indexes (get :access_token) keys set)
+            refresh-token-keys (-> @repo :indexes (get :refresh_token) keys set)]
+        (is (= 3 (count token-keys)) "Should have 3 tokens total")
+        (is (= 5 (count refresh-token-keys)) "Should have 5 refresh tokens total")
+        (is (= #{"token-3" "token-4" "token-5"} token-keys) "Should have correct tokens")
+        (is (= #{"refresh-token-2" "refresh-token-3" "refresh-token-4" "refresh-token-5" "refresh-token-6"} refresh-token-keys) "Should have correct refresh tokens")))
+
+    ;; Load repository from file again and compare
+    (testing "Repository reloaded from file after adding more entries"
+      (let [loaded-repo (token/create-token-repository "tokens1.nosql")]
+        (assert-repositories-match @repo loaded-repo 3 5)))))
+
+
+(deftest test-repository-synchronization
+  (let [repo1 (atom (token/create-token-repository "tokens1.nosql"))
+        repo2 (atom (token/create-token-repository "tokens2.ndjson"))
+        read-repo1 (atom nil)
+        read-repo2 (atom nil)]
+    
+    ;; Create test data
+    (doseq [i (range 1 5)]
+      (create-example-token! repo1 i)
+      (create-example-refresh-token! repo1 i)
+      (create-example-token! repo2 i)
+      (create-example-refresh-token! repo2 i))
+
+    ;; Initialize read repositories
+    (reset! read-repo1 (token/create-token-repository "tokens1.nosql"))
+    (reset! read-repo2 (token/create-token-repository "tokens2.ndjson"))
+
+    ;; Test individual repository synchronization
+    (testing "Individual repository synchronization"
+      (assert-repositories-match @repo1 @read-repo1 4 4)
+      (assert-repositories-match @repo2 @read-repo2 4 4))
+
+    ;; Test cross-repository synchronization
+    (testing "Cross-repository synchronization"
+      (assert-repositories-match @repo1 @read-repo2 4 4))))
