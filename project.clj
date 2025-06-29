efd2a81a5b125cc3781b6a92923ce84372e4b528
Pierre Jacomet <pierrj@hotmail.com>
2025-06-29 17:43:42 -0300

    Aded jar exclusions

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
-(defproject mi-app-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.luv2c0d3/ndjson-repository "0.1.0-SNAPSHOT"
+  :description "A Clojure library for NDJSON-based repositories with O(1) lookups through in-memory indexing"
+  :url "https://github.com/your-username/ndjson-repository"
   :license {:name "EPL-2.0 OR GPL-2.0-with-classpath-exception"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.logging "1.2.4"]
                  [cheshire "5.12.0"]]
-  :main ^:skip-aot mi-app-clj.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :jar-exclusions [#"\.nosql$"]    ; Exclude test database files
+  :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.1"]]}}
+  :deploy-repositories [["releases" :clojars]
+                       ["snapshots" :clojars]])
